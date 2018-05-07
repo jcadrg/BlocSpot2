0dd4d99cbcd301e7e30d8f8beff98bbbfd42bfb0
Juanca <jcadrg@gmail.com>
2018-05-07 00:40:06 -0400

    zoom works correctly

--- com.jcadrg.BlocSpot2/com.jcadrg.BlocSpot2/MapViewController.swift
+++ com.jcadrg.BlocSpot2/com.jcadrg.BlocSpot2/MapViewController.swift
@@ -50,7 +50,7 @@ class MapViewController: UIViewController, UINavigationControllerDelegate, CLLoc
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations.last as! CLLocation
         let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
         self.mapView.setRegion(region, animated: true)
         
     }
