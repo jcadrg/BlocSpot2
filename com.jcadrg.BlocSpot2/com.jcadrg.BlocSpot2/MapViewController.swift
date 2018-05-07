7ae64f3ea9aef0f6ada6bf97f4cbfde2c3ef54b1
Juanca <juanca@Juancas-MacBook-Pro.local>
2018-05-07 00:19:13 -0400

    map view controller working, needs to zoom more

--- com.jcadrg.BlocSpot2/com.jcadrg.BlocSpot2/MapViewController.swift
+++ com.jcadrg.BlocSpot2/com.jcadrg.BlocSpot2/MapViewController.swift
@@ -9,13 +9,50 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController, UINavigationControllerDelegate {
+class MapViewController: UIViewController, UINavigationControllerDelegate, CLLocationManagerDelegate, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         navigationController?.delegate = self
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        
+        //Check for location services
+        if (CLLocationManager.locationServicesEnabled()) {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.requestWhenInUseAuthorization()
+        }
+        locationManager.requestWhenInUseAuthorization()
+        if(CLLocationManager.locationServicesEnabled()){
+            locationManager.startUpdatingLocation()
+        }
+        
+        //Zoom to user location
+        let noLocation = CLLocationCoordinate2D()
+        let viewRegion = MKCoordinateRegionMakeWithDistance(noLocation, 300, 300)
+        mapView.setRegion(viewRegion, animated: false)
+        
+        print(mapView.userLocation.location)
+        
+        DispatchQueue.main.async {
+            self.locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last as! CLLocation
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+        self.mapView.setRegion(region, animated: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
